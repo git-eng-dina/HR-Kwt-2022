550d22cef83254d02ce21b8c7c77ce4530aacc9e
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2022-11-24 12:51:15 +0200

    .

---
+++ Human_Resource/Human_Resource/App_Code/LinkCss.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Human_Resource.App_Code
+{
+    public class LinkCss
+    {
+        public static void AddCss(string path, Page page)
+        {
+            Literal cssFile = new Literal() { Text = @"<link href=""" + page.ResolveUrl(path) + @""" type=""text/css"" rel=""stylesheet"" />" };
+            page.Header.Controls.Add(cssFile);
+        }
+    }
+}
