970bfdd6b656b703f91448671fd47b06606eea43
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-11-25 04:56:27 +0300

    1

---
+++ Human_Resource1/Human_Resource/login.aspx.cs
@@ -0,0 +1,61 @@
+﻿using Human_Resource.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Human_Resource
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataBind();
+        }
+        protected void login_Click(object sender, EventArgs e)
+        {
+
+            EmployeeModel user = new EmployeeModel();
+            string passwordStr = HelpClass.MD5Hash("Inc-m" + password.Value);
+            var loginUser = user.GetUser(username.Value, password.Value);
+
+            if (loginUser == null)
+            {
+                lbl_error.Visible = true;
+                lbl_error.Style.Add("display", "block");
+            }
+            else
+            {
+                Session["user_id"] = loginUser.EmployeeID;
+
+                //FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket
+                //              (
+                //              1,
+                //              loginUser.Username,
+                //              DateTime.Now,
+                //              DateTime.Now.AddMinutes(15), // expiry
+                //              false,
+                //              "",
+                //              "/"
+                //              );
+
+                ////encrypt the ticket and add it to a cookie
+                //string enTicket = FormsAuthentication.Encrypt(authTicket);
+                //HttpCookie cookie = new HttpCookie("POSCookie", enTicket);
+                //cookie.Expires = DateTime.Now.AddMinutes(15);
+                //cookie.HttpOnly = false;
+                //cookie.Values.Add("UserName", HttpUtility.UrlEncode(loginUser.Username));
+                //cookie.Values.Add("UserId", loginUser.EmployeeID.ToString());
+
+
+                //Response.Charset = "UTF-8";
+                //Response.Cookies.Add(cookie);
+                Response.Redirect("Views/Home/HomePage.aspx");
+
+            }
+        }
+    }
+}
