879f73559a9eca98ea65d68acbdd7d092b7fc0f1
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-11-29 22:57:24 +0300

    Update getData.cs

--- Human_Resource/Human_Resource/App_Code/getData.cs
+++ Human_Resource/Human_Resource/App_Code/getData.cs
@@ -19,20 +19,44 @@ namespace Human_Resource.App_Code
         #endregion
 
         #region MaritalStatus
-         static public List<KeyValueString> maritalStatusList;
-        IEnumerable<KeyValueString> getMaritalStatusList(string lang)
+         static public List<KeyValueString> maritalStatusList = getMaritalStatusList().ToList();
+        static IEnumerable<KeyValueString> getMaritalStatusList()
         {
              maritalStatusList = new List<KeyValueString> {
-                   new KeyValueString { key = "Soltero"       , value = ""  },
-                   new KeyValueString { key =   "Casado", value = "" },
-                   new KeyValueString { key =   "Absoluto", value = "" },
-                   new KeyValueString { key =   "Viudo", value = "" },
+                   new KeyValueString { key = "Soltero"       , value = Resources.Labels.Soltero  },
+                   new KeyValueString { key =   "Casado", value = Resources.Labels.Casado },
+                   new KeyValueString { key =   "Absoluto", value = Resources.Labels.Absoluto },
+                   new KeyValueString { key =   "Viudo", value = Resources.Labels.Viudo },
                     };
 
             return maritalStatusList;
         }
-
-
+        #endregion
+        #region custodiesType
+        static public List<KeyValueString> custodiesTypeList = getCustodiesTypeList().ToList();
+        static IEnumerable<KeyValueString> getCustodiesTypeList()
+        {
+            custodiesTypeList = new List<KeyValueString> {
+                   new KeyValueString { key = "Physical"       , value = Resources.Labels.Physical  },
+                   new KeyValueString { key =   "Cash", value = Resources.Labels.Cash },
+                   new KeyValueString { key =   "Car", value = Resources.Labels.Car },
+                   new KeyValueString { key =   "Other", value = Resources.Labels.Other },
+                    };
+            return custodiesTypeList;
+        }
+        #endregion
+        #region repeatType
+        static public List<KeyValueString> repeatTypeList = getRepeatTypeList().ToList();
+        static IEnumerable<KeyValueString> getRepeatTypeList()
+        {
+            repeatTypeList = new List<KeyValueString> {
+                   new KeyValueString { key = "Daily"       , value = Resources.Labels.Daily  },
+                   new KeyValueString { key =   "Weekly", value = Resources.Labels.Weekly },
+                   new KeyValueString { key =   "Monthly", value = Resources.Labels.Monthly },
+                   new KeyValueString { key =   "Annual", value = Resources.Labels.Annual },
+                    };
+            return repeatTypeList;
+        }
         #endregion
 
     }
