24ceef4a0815e59c55f836c13d5fa6b94640d7df
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-12-01 00:42:10 +0300

    training

---
+++ Human_Resource/Human_Resource/App_Code/Training.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Human_Resource.App_Code
+{
+    public class TrainingModel
+    {
+
+        #region Attributes
+        public int TrainingID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Notes { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<int> CreateUserID { get; set; }
+        public Nullable<int> UpdateUserID { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        #endregion
+
+        #region methods
+        public List<TrainingModel> getActivity()
+        {
+            using (HRSystemEntities entity = new HRSystemEntities())
+            {
+                var depts = entity.trainings.Where(x => x.IsActive == true)
+                                .Select(x => new TrainingModel()
+                                {
+                                    TrainingID = x.TrainingID,
+                                    Name = x.Name,
+                                    Description = x.Description,
+                                    CreateUserID = x.CreateUserID,
+                                    UpdateUserID = x.UpdateUserID,
+                                    Notes = x.Notes,
+                                    CreateDate = x.CreateDate,
+                                    UpdateDate = x.UpdateDate,
+                                }).ToList();
+                return depts;
+            }
+        }
+        public TrainingModel getTraining(int trainingId)
+        {
+            using (HRSystemEntities entity = new HRSystemEntities())
+            {
+                var dept = entity.trainings.Where(x => x.TrainingID == trainingId)
+                                .Select(x => new TrainingModel()
+                                {
+                                    TrainingID = x.TrainingID,
+                                    Name = x.Name,
+                                    Description = x.Description,
+                                    CreateUserID = x.CreateUserID,
+                                    UpdateUserID = x.UpdateUserID,
+                                    Notes = x.Notes,
+                                    CreateDate = x.CreateDate,
+                                    UpdateDate = x.UpdateDate,
+                                }).FirstOrDefault();
+                return dept;
+            }
+        }
+
+        public int SaveDept(TrainingModel dept)
+        {
+            try
+            {
+                trainings training;
+
+                using (HRSystemEntities entity = new HRSystemEntities())
+                {
+                    if (dept.TrainingID.Equals(0))
+                    {
+                        training = new trainings()
+                        {
+                            Name = dept.Name,
+                            Description = dept.Description,
+                            IsActive = true,
+                            CreateUserID = dept.CreateUserID,
+                            UpdateUserID = dept.UpdateUserID,
+                            CreateDate = DateTime.Now,
+                            UpdateDate = DateTime.Now,
+                        };
+                        training = entity.trainings.Add(training);
+                    }
+                    else
+                    {
+                        training = entity.trainings.Find(dept.TrainingID);
+                        training.Name = dept.Name;
+                        training.Description = dept.Description;
+                        training.Notes = dept.Notes;
+                        training.IsActive = true;
+                        training.UpdateUserID = dept.UpdateUserID;
+                        training.UpdateDate = DateTime.Now;
+                    }
+                    entity.SaveChanges();
+                }
+                return training.TrainingID;
+            }
+
+            catch
+            {
+                return 0;
+            }
+        }
+        public bool DeleteDept(int deptId, int? userId)
+        {
+            try
+            {
+                using (HRSystemEntities entity = new HRSystemEntities())
+                {
+                    var dept = entity.trainings.Find(deptId);
+                    dept.IsActive = false;
+                    dept.UpdateDate = DateTime.Now;
+                    dept.UpdateUserID = userId;
+                    entity.SaveChanges();
+                }
+                return true;
+            }
+
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
--- Human_Resource/Human_Resource/Views/ExecutiveProc/Trainings.aspx.cs
+++ Human_Resource/Human_Resource/Views/ExecutiveProc/Trainings.aspx.cs
@@ -1,17 +1,125 @@
-﻿using System;
+﻿using Human_Resource.App_Code;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Human_Resource.Views.ExecutiveProc
 {
     public partial class Trainings : System.Web.UI.Page
-    {
+     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindData();
+                btn_new.Attributes.Add("OnClick", "ShowDialog('');");
+            }
+        }
 
+        protected void btn_Search_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string textSearch = txt_search.Value;
+                BindData(textSearch);
+            }
+            catch { }
+        }
+        private void BindData(string textSearch = "")
+        {
+            TrainingModel dept = new TrainingModel();
+
+            var depts = dept.getActivity();
+            if (textSearch != "")
+                depts = depts.Where(x =>   x.Name.Contains(textSearch)
+                                || x.Description.Contains(textSearch)
+                                 ).ToList();
+            gv_data.DataSource = depts;
+
+            DataBind();
+        }
+        [WebMethod(EnableSession = true)]
+        public static string SaveTraining(string trainingId, string name, string description)
+        {
+            try
+            {
+                TrainingModel dept = new TrainingModel();
+                if (trainingId != "")
+                    dept.TrainingID = int.Parse(trainingId);
+                else
+                    dept.TrainingID = 0;
+                dept.Name = name;
+                dept.Description = description;
+
+                if (HttpContext.Current.Session["user_id"] != null && HttpContext.Current.Session["user_id"].ToString() != "")
+                    dept.CreateUserID = dept.UpdateUserID = int.Parse(HttpContext.Current.Session["user_id"].ToString());
+
+
+                int deptId = dept.SaveDept(dept);
+                if (deptId != 0)
+                {
+                    return "1";
+                }
+                return "0";
+            }
+            catch
+            {
+                return "0";
+
+            }
+
+        }
+
+        [WebMethod]
+        public static TrainingModel GetTraining(string ID)
+        {
+            try
+            {
+                TrainingModel dept = new TrainingModel();
+
+                int trainingId = int.Parse(ID);
+                dept = dept.getTraining(trainingId);
+
+                return dept;
+            }
+            catch
+            {
+                return null;
+
+            }
+
+        }
+        protected void deletedatafromgrid(object sender, CommandEventArgs e)
+        {
+
+            try
+            {
+                TrainingModel dept = new TrainingModel();
+                int Ref = Convert.ToInt32(e.CommandArgument.ToString());
+
+                int? userId = null;
+                if (Session["user_id"] != null && Session["user_id"].ToString() != "")
+                    userId = dept.UpdateUserID = int.Parse(Session["user_id"].ToString());
+
+                if (dept.DeleteDept(Ref, userId))
+                {
+
+                    Response.Write("<script>alert('" + Resources.Labels.DeleteSuccessfully + "')</script>");
+                    BindData();
+                }
+                else
+                {
+
+                    Response.Write("<script>alert('" + Resources.Labels.ErrorOccured + "')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+            }
         }
     }
 }
