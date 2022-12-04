16e33cf43f5fd9b8a069a3c662413d835a0eeb4d
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2022-12-04 18:22:53 +0200

    event

---
+++ Human_Resource/Human_Resource/App_Code/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Human_Resource.App_Code
+{
+    public class EventModel
+    {
+        #region Attributes
+        public int EventID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Notes { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<int> CreateUserID { get; set; }
+        public Nullable<int> UpdateUserID { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        #endregion
+
+
+        #region Methods
+        #endregion
+    }
+}
