0263cf6bebada47b8b091b5338ef41a70ba1a0d7
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-12-15 01:18:01 +0300

    Create EmployeesVacation.cs

---
+++ Human_Resource/Human_Resource/App_Code/EmployeesVacation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Human_Resource.App_Code
+{
+    public class EmployeesVacation
+    {
+    }
+}
