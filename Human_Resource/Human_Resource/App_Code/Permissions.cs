6b5cd2f5b4f2430f10ee4871a87634b36a9b4640
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-01-17 21:11:04 +0200

    .

---
+++ Human_Resource/Human_Resource/App_Code/Permissions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Human_Resource.App_Code
+{
+    public class appObjectsModel
+    {
+        #region Attributes
+        public int AppObjectID { get; set; }
+        public string Name { get; set; }
+        public string NameEn { get; set; }
+        public string NameAr { get; set; }
+        #endregion
+
+        #region Methods
+
+        public List<appObjects> GetAppObjects()
+        {
+            List<appObjects> result;;
+           
+
+            using (HRSystemEntities entity = new HRSystemEntities())
+            {
+                result = entity.appObjects.ToList();
+
+                return result;
+            }
+        }
+        #endregion
+    }
+    public class Permissions
+    {
+    }
+}
