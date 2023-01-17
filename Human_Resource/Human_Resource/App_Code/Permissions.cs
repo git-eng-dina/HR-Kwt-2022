f26cd16e2e7c5ca2daf8ed7f390851ea40e1e6f4
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2023-01-17 23:26:09 +0300

    Update Permissions.cs

--- Human_Resource/Human_Resource/App_Code/Permissions.cs
+++ Human_Resource/Human_Resource/App_Code/Permissions.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace Human_Resource.App_Code
 {
-    public class appObjectsModel
+    public class AppObjectModel
     {
         #region Attributes
         public int AppObjectID { get; set; }
@@ -28,9 +28,159 @@ namespace Human_Resource.App_Code
                 return result;
             }
         }
+
+         public AppObjectModel getAppObject(int appObjectId)
+        {
+            using (HRSystemEntities entity = new HRSystemEntities())
+            {
+                var appObject = entity.appObjects.Where(x => x.AppObjectID == appObjectId)
+                                .Select(x => new AppObjectModel()
+                                {
+                                    AppObjectID = x.AppObjectID,
+                                    Name = x.Name,
+                                    NameEn = x.NameEn,
+                                    NameAr = x.NameAr,
+                                }).FirstOrDefault();
+                return appObject;
+            }
+        }
+
         #endregion
     }
-    public class Permissions
+    public class UsersPermissionModel
     {
+        #region Attributes
+        public int UsersPermissionID { get; set; }
+        public int AppObjectID { get; set; }
+        public string AppObjectName { get; set; }
+        public long EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public Nullable<bool> ViewObject { get; set; }
+        public Nullable<bool> EditObject { get; set; }
+        public string Notes { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserID { get; set; }
+        public Nullable<long> UpdateUserID { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        #endregion
+
+        #region methods
+        public List<UsersPermissionModel> getAll()
+        {
+            using (HRSystemEntities entity = new HRSystemEntities())
+            {
+                var depts = entity.usersPermissions.Where(x => x.IsActive == true)
+                                .Select(x => new UsersPermissionModel()
+                                {
+                                    UsersPermissionID = x.UsersPermissionID,
+                                    AppObjectID = x.AppObjectID.Value,
+                                    AppObjectName = entity.appObjects.Where(m => m.AppObjectID == x.AppObjectID).Select(m => m.NameEn).FirstOrDefault(),
+                                    EmployeeID = x.EmployeeID.Value,
+                                    EmployeeName = entity.employees.Where(m => m.EmployeeID == x.EmployeeID).Select(m => m.NameEn).FirstOrDefault(),
+                                    ViewObject = x.ViewObject,
+                                    EditObject = x.EditObject,
+                                    CreateUserID = x.CreateUserID,
+                                    UpdateUserID = x.UpdateUserID,
+                                    Notes = x.Notes,
+                                    CreateDate = x.CreateDate,
+                                    UpdateDate = x.UpdateDate,
+                                }).ToList();
+                return depts;
+            }
+        }
+        public UsersPermissionModel getUsersPermission(int usersPermissionId)
+        {
+            using (HRSystemEntities entity = new HRSystemEntities())
+            {
+                var dept = entity.usersPermissions.Where(x => x.UsersPermissionID == usersPermissionId)
+                                .Select(x => new UsersPermissionModel()
+                                {
+                                    UsersPermissionID = x.UsersPermissionID,
+                                    AppObjectID = x.AppObjectID.Value,
+                                    AppObjectName = entity.appObjects.Where(m => m.AppObjectID == x.AppObjectID).Select(m => m.NameEn).FirstOrDefault(),
+                                    EmployeeID = x.EmployeeID.Value,
+                                    EmployeeName = entity.employees.Where(m => m.EmployeeID == x.EmployeeID).Select(m => m.NameEn).FirstOrDefault(),
+                                    ViewObject = x.ViewObject,
+                                    EditObject = x.EditObject,
+                                    CreateUserID = x.CreateUserID,
+                                    UpdateUserID = x.UpdateUserID,
+                                    Notes = x.Notes,
+                                    CreateDate = x.CreateDate,
+                                    UpdateDate = x.UpdateDate,
+                                }).FirstOrDefault();
+                return dept;
+            }
+        }
+
+        public int SaveDept(UsersPermissionModel dept)
+        {
+            try
+            {
+                usersPermissions usersPermission;
+
+                using (HRSystemEntities entity = new HRSystemEntities())
+                {
+                    if (dept.UsersPermissionID.Equals(0))
+                    {
+                        usersPermission = new usersPermissions()
+                        {
+                            AppObjectID = dept.AppObjectID,
+                            EmployeeID = dept.EmployeeID,
+                            ViewObject = dept.ViewObject,
+                            EditObject = dept.EditObject,
+
+                            IsActive = true,
+                            CreateUserID = dept.CreateUserID,
+                            UpdateUserID = dept.UpdateUserID,
+                            CreateDate = DateTime.Now,
+                            UpdateDate = DateTime.Now,
+                        };
+                        usersPermission = entity.usersPermissions.Add(usersPermission);
+                    }
+                    else
+                    {
+                        usersPermission = entity.usersPermissions.Find(dept.UsersPermissionID);
+                        usersPermission.AppObjectID = dept.AppObjectID;
+                        usersPermission.EmployeeID = dept.EmployeeID;
+                        usersPermission.ViewObject = dept.ViewObject;
+                        usersPermission.EditObject = dept.EditObject;
+
+                        usersPermission.Notes = dept.Notes;
+                        usersPermission.IsActive = true;
+                        usersPermission.UpdateUserID = dept.UpdateUserID;
+                        usersPermission.UpdateDate = DateTime.Now;
+                    }
+                    entity.SaveChanges();
+                }
+                return usersPermission.UsersPermissionID;
+            }
+
+            catch
+            {
+                return 0;
+            }
+        }
+        public bool DeleteDept(int deptId, long? userId)
+        {
+            try
+            {
+                using (HRSystemEntities entity = new HRSystemEntities())
+                {
+                    var dept = entity.usersPermissions.Find(deptId);
+                    dept.IsActive = false;
+                    dept.UpdateDate = DateTime.Now;
+                    dept.UpdateUserID = userId;
+                    entity.SaveChanges();
+                }
+                return true;
+            }
+
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
     }
 }
